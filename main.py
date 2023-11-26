5895d642dc34fe20fbe31f3e9c03bb4b73f2e503
technoKing <test@example.com>
2023-11-26 10:36:39 +0100

    fixed syntax error

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def check_winner(board, player):
         if all(board[row][col] == player for row in range(3)):
             return True
 
-    if all(board[i][i] == player for i in range(3))) or all(board[i][2 - i] == player for i in range(3)):
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
         return True
 
     return False
